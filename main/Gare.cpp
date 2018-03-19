6de2ddcdf46562786f92b43b85224847488f5a46
Theo Berger <theo.berger.20160848@efrei.net>
2018-03-19 16:03:20 +0100

    add Line object

--- main/Gare.cpp
+++ main/Gare.cpp
@@ -1,7 +1,11 @@
 #include "Gare.h"
+using namespace std;
 
-Gare::Gare()
+Gare::Gare(string _nom, vector<Ligne*> _lignes)
 {
+    nom = _nom;
+    lignes = _lignes;
+    voyageurs = vector<int>();
 }
 
 Gare::~Gare()
--- main/Gare.h
+++ main/Gare.h
@@ -1,16 +1,21 @@
 #pragma once
 #ifndef GARE_H_INCLUDED
 #define GARE_H_INCLUDED
+#include<string>
+#include<vector>
+#include "Ligne.h"
+
 
 class Gare
 {
 public:
-	Gare();
+    Gare(std::string _nom, std::vector<Ligne*> _lignes);
 	~Gare();
 
 private:
-
-
+    std::string nom;
+    std::vector<int> voyageurs;
+    std::vector<Ligne*> lignes;
 
 
 

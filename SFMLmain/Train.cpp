29116c580d94170441cd9ea1275f6d3595786827
UnknownScientist <maxence.fossat@efrei.net>
2018-03-25 21:40:17 +0200

    enlevé std superflus
    Dans Ligne.cpp et Train.cpp des std:: alors qu'il y a la ligne using namespace std

--- SFMLmain/Train.cpp
+++ SFMLmain/Train.cpp
@@ -252,7 +252,7 @@ float distf(pair<float,float> g_a, pair<float,float> g_b)
 
 }
 
-std::pair<float,float> Train::getCoords() const
+pair<float,float> Train::getCoords() const
 {
     return coords;
 }
